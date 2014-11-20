b45e7dccbfe0023537e05bb77842053fef88ad0e
jlopezmoliner <j.lopezmoliner@ub.edu>
2014-11-20 13:41:11 +0100

    amend list para

--- R/curve_psy.R
+++ R/curve_psy.R
@@ -5,6 +5,6 @@ curve_psy <- function(l, xmin, xmax) {
   if (missing(xmin)) xmin <- min(l$d[[x]])
   if (missing(xmax)) xmax <- max(l$d[[x]])
   xseq <- seq(xmin, xmax, length = 100)
-  yseq <- l$psy_fun(xseq, para)
+  yseq <- l$psy_fun(xseq, l$para)
   data.frame(x = xseq, y= yseq)
 }
