783642beaca057d4fa42d430f652335d6c933790
danilinares <danilinares@gmail.com>
2014-11-25 15:05:46 +0100

    1

--- R/quickpsy.R
+++ R/quickpsy.R
@@ -6,29 +6,27 @@ quickpsy <- function(d, x, k, n, random, within, between, psy_fun_name,
   k <- deparse(substitute(k))
   n <- deparse(substitute(n))
   grouping_var <- c()
-  if (!(missing(random) && missing(within) && missing(between))) {
-    if (!missing(random)) {
-      random <- deparse(substitute(random))
-      grouping_var <- c(grouping_var, random)
-    }
-    if (!missing(within)) {
-      within <- deparse(substitute(within))
-      grouping_var <- c(grouping_var, within)
-    }
-    if (!missing(between)) {
-      random <- deparse(substitute(between))
-      grouping_var <- c(grouping_var, between)
-    }
-    print(grouping_var)
-    d <- d %>% group_by_(.dots=grouping_var)
+
+  if (!missing(random)) {
+    random <- deparse(substitute(random))
+    grouping_var <- c(grouping_var, random)
   }
+  if (!missing(within)) {
+    within <- deparse(substitute(within))
+    grouping_var <- c(grouping_var, within)
+  }
+  if (!missing(between)) {
+    between <- deparse(substitute(between))
+    grouping_var <- c(grouping_var, between)
+  }
+
+  d <- d %>% group_by_(.dots=grouping_var)
 
   fits <- d %>%
     do(fit=fit_psy(., x, k, n, psy_fun_name, pini, guess=guess, lapses=lapses))
 
-  fits
-  #curve <-  plyr::ddply(fits,grouping_var, curve_psy)
-  #para <-  plyr::ddply(fits,grouping_var, para_psy)
+  curve <-  plyr::ddply(fits,grouping_var, curve_psy)
+  para <-  plyr::ddply(fits,grouping_var, para_psy)
 
   list(curve = curve , para = para)
 }
