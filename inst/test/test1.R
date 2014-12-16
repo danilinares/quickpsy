280e04f03fa8a035da7c02d26f430009cc35e951
danilinares <danilinares@gmail.com>
2014-12-16 17:14:13 +0100

    1

--- inst/test/test1.R
+++ inst/test/test1.R
@@ -3,18 +3,12 @@ library(ggplot2)
 library(quickpsy)
 
 dat <- read.table('inst/extdata/lopezmolinerlinares2006.txt', header = TRUE)
-av <- dat %>% group_by(phase, subject, speed, cond) %>%
-  summarise(ntrials = n(), nyes = sum(response), y = nyes / ntrials)
-fit <- quickpsy(av, phase, nyes, ntrials, random = .(subject), within = .(speed, cond))
-plotcurves(fit)
+av <- dat %>% group_by(phase, subject, speed, cond) %>% summarise(ntrials = n(), nyes = sum(response), y = nyes / ntrials)
+fit <- quickpsy(av, phase, nyes, ntrials,within = .(speed,subject, cond))
 
-
-av <- dat %>% group_by(phase, speed, cond) %>%
-  summarise(ntrials = n(), nyes = sum(response), y = nyes / ntrials)
+av <- dat %>% group_by(phase, speed, cond) %>% summarise(ntrials = n(), nyes = sum(response), y = nyes / ntrials)
 fit <- quickpsy(av, phase, nyes, ntrials,  within = .(speed, cond))
-plotcurves(fit)
 
-av <- dat %>% group_by(phase, cond) %>%
-  summarise(ntrials = n(), nyes = sum(response), y = nyes / ntrials)
+av <- dat %>% group_by(phase, cond) %>% summarise(ntrials = n(), nyes = sum(response), y = nyes / ntrials)
 fit <- quickpsy(av, phase, nyes, ntrials,  within = .( cond))
-plotcurves(fit)
+
