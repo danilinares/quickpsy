7b28b9e984ba1ff80298d11c6b26c5dd2fde2925
danilinares <danilinares@gmail.com>
2014-12-16 17:09:32 +0100

    adding plots

--- inst/test/test1.R
+++ inst/test/test1.R
@@ -3,54 +3,18 @@ library(ggplot2)
 library(quickpsy)
 
 dat <- read.table('inst/extdata/lopezmolinerlinares2006.txt', header = TRUE)
-
-av <- dat %>% group_by(phase) %>%
+av <- dat %>% group_by(phase, subject, speed, cond) %>%
   summarise(ntrials = n(), nyes = sum(response), y = nyes / ntrials)
+fit <- quickpsy(av, phase, nyes, ntrials, random = .(subject), within = .(speed, cond))
+plotcurves(fit)
 
-fit <- quickpsy(av, phase, nyes, ntrials,
-                psy_fun = cum_normal_fun, pini=c(.5,.3))
 
-p <- ggplot(data=av,aes(x=phase,y=y))+
-  geom_point()+
-  geom_line(data=fit$curve)
-p
-
-av <- dat %>% group_by(phase, subject) %>%
+av <- dat %>% group_by(phase, speed, cond) %>%
   summarise(ntrials = n(), nyes = sum(response), y = nyes / ntrials)
+fit <- quickpsy(av, phase, nyes, ntrials,  within = .(speed, cond))
+plotcurves(fit)
 
-fit <- quickpsy(av, phase, nyes, ntrials, random = subject,
-                psy_fun = cum_normal_fun, pini=c(.5,.3))
-
-p <- ggplot(data=av,aes(x=phase,y=y))+
-  facet_wrap(~subject) +
-  geom_point()+
-  geom_line(data=fit$curve)
-p
-
-av <- dat %>% group_by(phase, subject, cond) %>%
+av <- dat %>% group_by(phase, cond) %>%
   summarise(ntrials = n(), nyes = sum(response), y = nyes / ntrials)
-
-fit <- quickpsy(av, phase, nyes, ntrials, random = subject, within = cond,
-                psy_fun = cum_normal_fun, pini=c(.5,.3))
-
-p <- ggplot(data=av,aes(x=phase,y=y, color=cond))+
-  facet_wrap(~subject) +
-  geom_point()+
-  geom_line(data=fit$curve)
-p
-
-av <- dat %>% group_by(phase, subject, cond, speed) %>%
-  summarise(ntrials = n(), nyes = sum(response), y = nyes / ntrials)
-
-fit <- quickpsy(av, phase, nyes, ntrials, random = subject, within = cond,
-                between = speed,
-                psy_fun = cum_normal_fun, pini=c(.5,.3))
-
-p <- ggplot(data=av,aes(x=phase,y=y, color=cond))+
-  facet_grid(speed~subject) +
-  geom_point()+
-  geom_line(data=fit$curve)
-p
-
-
-
+fit <- quickpsy(av, phase, nyes, ntrials,  within = .( cond))
+plotcurves(fit)
