cd99e8611da6038c2f2001fe56c19af5d74071cc
danilinares <danilinares@gmail.com>
2015-07-10 21:00:41 +0200

    Introducing pair comparisons between the estimates of the parameters using bootstrap.

---
+++ R/parcomparisons.R
@@ -0,0 +1,9 @@
+#' Pair comparisons of the parameters using bootstrap
+#' \code{parcomparisons} Calculates the bootstrap confidence intervals for the
+#' difference in the parameters for two groups for all possible pairs
+#' of groups
+#' @export
+parcomparisons <- function(qp, ci = .95) {
+  qp$parbootstrap %>% group_by_(.dots = 'parn') %>%
+    do(one_parcomparisons(., qp$par, qp$groups, ci))
+}
