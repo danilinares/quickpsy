1e308c14c5e8e81a50dd0803b257da2eb42b3217
danilinares <danilinares@gmail.com>
2016-06-03 07:41:39 +0200

    taking into account that the argument extension has been added

--- R/quickreadfiles.R
+++ R/quickreadfiles.R
@@ -20,6 +20,7 @@ quickreadfiles <- function(path = getwd(), extension = 'txt', ...) {
 
   arguments <- c(as.list(environment()), list(...))
   arguments[1] <- NULL
+  arguments[1] <- NULL
   exist<-NULL # Joan added 1-4-2015
 
   if (extension == 'txt') {
