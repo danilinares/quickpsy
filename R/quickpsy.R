a48c511cd1c46faa52066c6c0c855ef0c4af361c
daniel linares <daniellinares@daniels-mbp.csc.es>
2017-07-04 17:40:30 +0200

    1

--- R/quickpsy.R
+++ R/quickpsy.R
@@ -1,4 +1,4 @@
-#' Fits psychometric functions
+#' Fits psychometric functionsss
 #'
 #' \code{quickpsy} fits, by direct maximization of the likelihood
 #' (Prins and Kingdom, 2010; Knoblauch and Maloney, 2012),
